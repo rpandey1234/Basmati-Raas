f2e2aeea564a8305bfe784bc11922f84ae1a624e
Rahul Pandey <rpandey1234@gmail.com>
2012-09-12 05:31:49 -0700

    fix migration

--- db/migrate/20120909022711_load_data.rb
+++ db/migrate/20120909022711_load_data.rb
@@ -1,12 +1,14 @@
 class LoadData < ActiveRecord::Migration
   def up
-  	pandey = Users.new(:first_name => "Rahul", :last_name => "Pandey", :year => "2013", :bio => "I like to dance and smile and listen!")
-  	pandey.save(:validate => false)
-  	doshi = Users.new(:first_name => "Tulsee", :last_name => "Doshi", :year => "2015", :bio =>"Dancing and singing all the way")
-  	doshi.save(:validate => false)
+  	u1 = User.new(:first_name => "Rahul", :last_name => "Pandey", :year => "2013", :bio => "I like to dance and smile and listen!")
+  	u1.save(:validate => false)
+  	u2 = User.new(:first_name => "Ankita", :last_name => "Patro", :year => "2012", :bio =>"Dancing and singing all the way")
+  	u2.save(:validate => false)
+  	u3 = User.new(:first_name => "Shilpa", :last_name => "Sethuraman", :year => "2013", :bio =>"Ms. Captain")
+  	u3.save(:validate => false)
   end
 
   def down
-  	Users.delete_all
+  	User.delete_all
   end
 end
