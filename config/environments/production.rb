76639dc167663233222948e4339937eb0a04833a
Rahul Pandey <rpandey1234@gmail.com>
2012-09-17 21:54:05 -0700

    attempt to fix file uploading by changing compiling in prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ BasmatiRaas::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # changed 09/17/2012
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
